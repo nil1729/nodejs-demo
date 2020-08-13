5b766b15472849310a3ad04df2fd332a75312269
Nilanjan Deb <nilanjan172nsvian@gmail.com>
2020-08-13 11:45:19 +0530

    add middleware

--- pagination-api/server.js
+++ pagination-api/server.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const app = express();
-const users = require('./users');
+const { users, posts } = require('./data');
 
-app.get('/users', async (req, res) => {
-	res.json(users);
+const paginateResults = model => {
+	return (req, res, next) => {
+		const page = parseInt(req.query.page);
+		const limit = parseInt(req.query.limit);
+		const startIndex = (page - 1) * limit;
+		const endIndex = page * limit;
+		let results = {};
+		if (page > 1) {
+			results.previousPage = {
+				page: page - 1,
+				limit: limit,
+			};
+		}
+		if (endIndex < model.length) {
+			results.nextPage = {
+				page: page + 1,
+				limit: limit,
+			};
+		}
+		results.results = model.slice(startIndex, endIndex);
+		res.paginatedResults = results;
+		next();
+	};
+};
+
+app.get('/users', paginateResults(users), async (req, res) => {
+	res.json(res.paginatedResults);
+});
+
+app.get('/posts', paginateResults(posts), async (req, res) => {
+	res.json(res.paginatedResults);
 });
 
 const PORT = process.env.PORT || 5050;
